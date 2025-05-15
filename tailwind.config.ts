70eb42d04f15a378f48226451ea070576b5e5ff0
kartikshingde <kartikshingde12@gmail.com>
2025-05-15 11:52:17 +0530

    added heroSection,spotlight,movingBorder btn using aceternity

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,30 @@
-// tailwind.config.ts
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    ".src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    ".src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode:'class',
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
